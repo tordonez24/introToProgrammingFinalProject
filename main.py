64efab6b55b4e0b53b56b09c3bd0ba248882b27f
tordonez24 <tyler.m.ordonez@gmail.com>
2022-11-15 14:37:40 -0800

    outline p1

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+'''
+My final project is...
+'''
+
+'''
+Sources:
+'''
+
+# imported libraries
+import pygame as pg
+
+# built in libraries
+import tkinter as tk
+
+# created libraries
+import settings
+
 # my final main file
 import pygame as pg
+
+# global variables
+
+# utility functions
+
+# classes
