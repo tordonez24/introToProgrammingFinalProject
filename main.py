0b8b5343a6389626f6280c914c88beea938a138e
tordonez24 <tyler.m.ordonez@gmail.com>
2022-11-17 12:40:15 -0800

    changed to flappy fish

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 '''
-My final project is...
+My final project is Flappy Fish. Flappy Fish is a spinoff of Flappy Bird, which is a single player 2D game. The objective is to avoid the pillars that can kill you and get the highest possible score. Each pillar past gives you 1 point.
 '''
 
 '''
 Sources:
-I used w3schools to learn about machine learning libraries
+https://www.askpython.com/python/examples/flappy-bird-game-in-python
 '''
 
 # imported libraries
 import pygame as pg
 
 # built in libraries
-import tkinter as tk
+import random
+import sys
 
 # created libraries
 import settings
 
-# my final main file
-import pygame as pg
 
 # global variables
 
