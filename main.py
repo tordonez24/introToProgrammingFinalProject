545cfa3ff0d54ccd08fa528e130549c04eea96d1
tordonez24 <tyler.m.ordonez@gmail.com>
2022-12-03 11:13:12 -0800

    comments

--- main.py
+++ main.py
@@ -91,13 +91,13 @@ class Game:
                     self.fish.jump()
                 if event.type == self.pipe_timer:
                     Pipe([self.all_sprites, self.collision_sprites], self.scale_factor / 1.55)
-            # updating pygame and calling the framrate
+            # updating pygame
             self.display_surface.fill('black')
             self.all_sprites.update(delta_time) # updates sprites with delta time
             self.collisions()
             self.all_sprites.draw(self.display_surface) # draws sprites
             pg.display.update()
-            self.clock.tick(FPS)
+            self.clock.tick(FPS) # calling framrate
 
 class Background(Sprite):
     def __init__(self, groups, scale_factor):
@@ -110,7 +110,7 @@ class Background(Sprite):
         done_width = background_image.get_width() * scale_factor
         # transforms original image into fully sized image for background
         done_image = pg.transform.scale(background_image,(done_width, done_height))
-        # create surface twice as wide as original background image to make double background
+        # create surface twice as wide as original background image to make double background for final image
         self.image = pg.Surface((done_width * 2, done_height))
         # draws fully sized image at (0,0)
         self.image.blit(done_image,(0,0))
@@ -165,18 +165,21 @@ class Pipe(Sprite):
         orientation = choice(('bottom', 'top'))
         # loads pipe image
         pipe_image = pg.image.load(os.path.join(img_folder, 'pipe.png')).convert()
-        # scales pipe image
+        # scales pipe image into final pipe image
         self.image = pg.transform.scale(pipe_image,pg.math.Vector2(pipe_image.get_size())* scale_factor)
-
-        x = WIDTH + randint (50,90)
-
+        # x value is same for all pipes, but here, it chooses a random value to add to the x value to shift it some units to the left when it spawns
+        x = WIDTH + randint(50,90)
+        # determines how far pipe sticks out when pipe is on the ground
         if orientation == 'bottom':
             y = HEIGHT + randint(10,50)
+            # places image on middle right bottom of screen
             self.rect = self.image.get_rect(midbottom = (x,y))
-        else:
+        # determines how far pipe sticks out when on top
+        if orientation == 'top':
             y = 0 + randint(-50, -10)
-            # makes a chance for pipe to be flipped vertically, but not horizontally
+            # flips pipe vertically, but not horizontally
             self.image = pg.transform.flip(self.image, False, True)
+            # places image on middle right top of screen
             self.rect = self.image.get_rect(midtop = (x,y))
         self.pos = pg.math.Vector2(self.rect.topleft)
     # update method
